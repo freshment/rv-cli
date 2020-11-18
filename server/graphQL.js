b26de635819f3be3a4f749d39c72b919a063870a
GavinLam164 <m13590491852@163.com>
2020-11-18 15:42:14 +0800

    feat(graphql): 简单demo

--- server/graphQL.js
+++ server/graphQL.js
@@ -1,32 +1,30 @@
-
-const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
-
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const persons = require('./person.json')
+ 
 const typeDefs = gql`
-  type Query {
-    hello: String
+  type Person {
+    name: ID,
+    age: Int
   }
-`
-
+  type Query {
+    person(name: String): Person
+  }
+`;
+ 
 const resolvers = {
   Query: {
-    hello: () => 'hello world'
-  }
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-const app = express()
-
-server.applyMiddleware({
-  app
-})
-
-app.listen({
-  port: 4000
-}, () => {
-  console.log('graphSQL listen on: http://localhost:4000' + server.graphqlPath)
-})
+    person(root, { name }) {
+      return persons.find(it => it.name === name)
+    },
+  },
+};
+ 
+const server = new ApolloServer({ typeDefs, resolvers });
+ 
+const app = express();
+server.applyMiddleware({ app });
+ 
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
